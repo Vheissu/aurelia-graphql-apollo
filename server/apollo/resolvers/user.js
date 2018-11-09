e65ea590e65f9eb9baf920e064a14de39394692a
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-11-09 22:29:59 +1000

    feat(server): cleanup schemas and config

--- server/apollo/resolvers/user.js
+++ server/apollo/resolvers/user.js
@@ -2,7 +2,7 @@ import { ApolloError } from 'apollo-server-express';
 
 export default {
     Query: {
-        author: (_, { id }, { db }) => db.author.findById(id) 
+        user: (_, { id }, { db }) => db.author.findById(id) 
     },
     User: {
         posts: (_, args, context) => _.getPosts(),
--- server/apollo/schema/user.js
+++ server/apollo/schema/user.js
@@ -2,7 +2,6 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
     extend type Query {
-        users: [User!]
         user(id: ID!): User
         posts: [Post!]!
         me: User

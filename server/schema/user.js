a7811e45a5e9fa0ccd4cea167a65adb71a6b801b
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-11-09 10:37:56 +1000

    fix(schema): use node.js syntax

--- server/schema/user.js
+++ server/schema/user.js
@@ -1,6 +1,6 @@
-import { gql } from 'apollo-server-express';
+const { gql } = require('apollo-server-express');
 
-export default gql`
+module.exports = gql`
   extend type Query {
     users: [User!]
     user(id: ID!): User
